ded5f826992c0bdbd0055905cc61abbc216babf4
Lumeraaa <tyoardana7@gmail.com>
2025-12-16 10:00:07 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ st.write("Model: TF-IDF + Linear SVM")
 
 text = st.text_area("Masukkan teks:")
 
-if st.button("Prediksi"):
-    if pred == 0:
+pred = model.predict(X_input)[0]
+
+if pred == 0:
     label = "Mental Health Discourse / Potential Distress"
+    st.error(f"Hasil: {label}")
 else:
     label = "Neutral / No Distress Indicators"
+    st.success(f"Hasil: {label}")
 
